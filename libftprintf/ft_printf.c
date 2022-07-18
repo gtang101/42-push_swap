e0f5f951f936001f9f3d4a7fcd01792a789d571a
Gary Tang <ktang@F2K004.42adel.org.au>
2022-07-18 14:54:32 +0930

    first_batch

---
+++ libftprintf/ft_printf.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 21:22:10 by ktang             #+#    #+#             */
+/*   Updated: 2022/05/18 17:03:24 by ktang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	int		count;
+	va_list	arg;
+
+	va_start(arg, str);
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == '%' && str[++i])
+		{
+			count += ft_char_type(str[i], arg, &i);
+			i++;
+		}
+		if (str[i] && str[i] != '%')
+		{
+			ft_putchar_fd(str[i], 1);
+			i++;
+			count++;
+		}
+	}
+	va_end(arg);
+	return (count);
+}
